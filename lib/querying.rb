8fe91067dc2a87e31394650c9260f41f8fcde1bb
Melissa Real <melissa.real@hotmail.com>
2018-10-02 13:23:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title, characters.species
   FROM characters
-  LEFT JOIN series ON characters.id = series.characters_id
+  LEFT JOIN series ON series.id = characters.series_id
   GROUP BY characters.name
   HAVING characters.species = 'human';"
 end
