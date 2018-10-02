cf6f54cb0bc30e69f5c326acff1a9aa4114dc800
Melissa Real <melissa.real@hotmail.com>
2018-10-02 13:50:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,9 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(characters.name)
   FROM characters
-  JOIN character_books ON character_books.book_id = character_books.character_id
-  JOIN books ON character_books.id = books.id
-  GROUP BY characters.name;"
+  JOIN character_books ON characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY COUNT(characters.name)
+  DESC
+  ;"
 end
