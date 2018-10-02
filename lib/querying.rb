3f86aa0bf21b0c42e82de8b5026ae899a7e50c59
Melissa Real <melissa.real@hotmail.com>
2018-10-02 13:31:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,5 +37,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, books.title
+  FROM characters
+  JOIN character_books ON character_books.book_id = character_books.character_id
+  JOIN books ON character_books.id;"
 end
