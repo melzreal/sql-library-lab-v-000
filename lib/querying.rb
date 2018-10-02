8275ecbcccb791c21e375a8a8f09ae84411f7700
Melissa Real <melissa.real@hotmail.com>
2018-10-02 13:15:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,10 +29,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species
+  "SELECT DISTINCT series.title, characters.species, COUNT(characters.species)
   FROM characters
-  LEFT JOIN series ON series.id = characters.series_id
-  ORDER BY COUNT(characters.species);"
+  JOIN series ON series.id = characters.series_id
+  ORDER BY COUNT(characters.species) DESC;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
